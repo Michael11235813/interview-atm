c283fd844e322af4b2fbd57f2996f89834e0e656
Michael11235813 <sack.michael@siemens.com>
2021-11-09 16:06:18 +0100

    1 file modified

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello NestJS!';
-  }
+  private readonly logger = new Logger(AppService.name);
+    private readonly databaseFilePath = 'database.json';
+
+    public changePin(oldPin: string, newPin: string, userId: string): void {
+        // ////////////////////
+        // Start Coding here //
+        // ////////////////////
+        let pnewPin = newPin.split('');
+        console.log(pnewPin);
+
+        for (var i = 0; i <= pnewPin.length; i++) {
+            let tmp2 = parseInt(pnewPin[i + 1]);
+            let tmp1 = parseInt(pnewPin[i]);
+
+            console.log(tmp1,tmp2);
+
+            if (tmp1 + 1 == tmp2) {
+                console.log("True");
+            } else {
+                console.log("False");
+            }
+
+        }
+
+        this.logger.debug(`Received old pin="${oldPin}", new pin="${newPin}" and userId="${userId}".`);
+
+        // //////////////////
+        // End Coding here //
+        // //////////////////
 }
